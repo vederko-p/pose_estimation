cd8f58472749d3c93eee10f48916ce1245de8bf9
Skibin Maksim <ski6a_23@mail.ru>
2022-11-13 19:06:54 +0300

    inference yolo script: minor filepath change

--- scripts/yolo_detect.sh
+++ scripts/yolo_detect.sh
@@ -1,7 +1,7 @@
 YOLO_DIR="../src/models/yolov7"
 WEIGHTS_PATH="/home/maksim/Desktop/Projects/neft_project/pose_estimation/src/models/weights/yolov7-w6-person.pt"
 
-FILEPATH="/home/maksim/Desktop/Projects/neft_project/pose_estimation/data/2_football.jpg"
+FILEPATH="/home/maksim/Desktop/Projects/neft_project/pose_estimation/data/first_test_data/3_assasins_short.mp4"
 
 cd $YOLO_DIR
 
