2add891efaa325ad821921e8c61a124c232b5305
Skibin Maksim <ski6a_23@mail.ru>
2022-11-25 01:00:48 +0300

    yolo pose script: make relative filepath

--- scripts/yolo_pose_detect.sh
+++ scripts/yolo_pose_detect.sh
@@ -1,7 +1,7 @@
 YOLO_DIR="../src/models/yolov7_pose"
-WEIGHTS_PATH="/home/skibinmv/Desktop/neft_project/pose_estimation/src/weights/yolov7_pose_estimation/yolov7-w6-pose.pt"
+WEIGHTS_PATH="../src/weights/yolov7_pose_estimation/yolov7-w6-pose.pt"
 
-FILEPATH="/home/skibinmv/Desktop/neft_project/pose_estimation/data/first_test_data/1_workshop_1.mp4"
+FILEPATH="../data/first_test_data/1_workshop_1.mp4"
 
 cd $YOLO_DIR
 
