9387fe0f75f43de9e0e3f4255292f728bb1e26e1
Skibin Maksim <ski6a_23@mail.ru>
2022-11-24 23:20:26 +0300

    download data script: take out download into utils

--- scripts/download_data.sh
+++ scripts/download_data.sh
@@ -12,16 +12,4 @@ default_data_names=(
   first_test_data
 )
 
-check_and_mkdir $OUTPUT_DIR
-cd $OUTPUT_DIR
-
-
-for i in ${!default_data_files_ids[*]}
-do
-  file_id=${default_data_files_ids[i]}
-  file_name=${default_data_names[i]}
-  echo Download $file_name dataset...
-  gdown $file_id
-  unzip "$file_name.zip" -d $file_name
-  rm "$file_name.zip"
-done
+download_from_google_drive $OUTPUT_DIR $default_data_files_ids $default_data_names
--- scripts/utils.sh
+++ scripts/utils.sh
@@ -18,3 +18,25 @@ install_requirements() {
     cd -
   fi
 }
+
+
+download_from_google_drive() {
+  local output_dir=$1
+  local files_ids=$2
+  local files_names=$3
+
+  check_and_mkdir $output_dir
+  cd $output_dir
+
+  for i in ${!files_ids[*]}
+  do
+    local file_id=${files_ids[i]}
+    local file_name=${files_names[i]}
+    echo Download $file_name...
+    gdown $file_id
+    unzip "$file_name.zip" -d $file_name
+    rm "$file_name.zip"
+  done
+
+  cd -
+}
